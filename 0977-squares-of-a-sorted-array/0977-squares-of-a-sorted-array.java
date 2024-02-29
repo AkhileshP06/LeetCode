dfddaadef6c377094fdeb303bd2ed4b521af8570
Akhilesh P <106602217+AkhileshP06@users.noreply.github.com>
2024-02-29 14:06:10 +0530

    Time: 321 ms (5.77%), Space: 47.2 MB (47.59%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -0,0 +1,33 @@
+class Solution 
+{
+    public int[] sortedSquares(int[] nums) 
+    {
+        int res[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            res[i]=nums[i]*nums[i];
+        }
+        return sort(res);
+    }
+    int[] sort(int arr[])
+    {
+        int n = arr.length;
+  
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+  
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
+    }
+}
